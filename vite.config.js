c0cf7088d2ad98d644e9e8f865848e0b48bae3cc
Zubery Pima <zuberypima12@gmail.com>
2026-02-12 00:18:32 +0300

    vite configurations done

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,23 @@ export default {
     root: './',
     build: {
         outDir: 'dist',
+        rollupOptions: {
+            input: {
+                main: 'index.html',
+                hardware: 'hardware-solutions.html',
+                loans: 'loan-management.html',
+                onboarding: 'customer-onboarding.html',
+                transactions: 'transaction-processing.html',
+                fraud: 'fraud-detection.html',
+                compliance: 'compliance-tools.html',
+                payment: 'payment-gateways.html',
+                mobile: 'mobile-wallets.html',
+                blockchain: 'blockchain-solutions.html',
+                networks: '5g-networks.html',
+                fiber: 'fiber-optics.html',
+                tower: 'tower-maintenance.html'
+            }
+        }
     },
     server: {
         open: true
